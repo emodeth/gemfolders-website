5f44353ee569e833c3b19295d839710b41660a88
Emirhan Keskin <emodethistaken@gmail.com>
2026-01-30 07:34:35 +0300

    feature: logo icon added to logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,19 @@
 import Link from "next/link";
+import Image from "next/image";
 
-const Logo = ({ className }: { className?: string }) => {
+const Logo = ({ className = "" }: { className?: string }) => {
   return (
-    <Link href="/" className="flex items-center">
-      <span className={`font-bold tracking-tight text-foreground ${className}`}>
-        gemfolders
+    <Link href="/" className={`flex items-center gap-2 ${className}`}>
+      <div className="relative h-8 w-8 shrink-0 overflow-hidden hidden min-[425px]:block">
+        <Image
+          src="/images/logo.png"
+          alt="Gemfolders Logo"
+          fill
+          className="object-cover"
+        />
+      </div>
+      <span className="font-bold tracking-tight text-foreground text-[16px] min-[425px]:text-xl">
+        Gemfolders
       </span>
     </Link>
   );
